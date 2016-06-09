01d49664ad2b67fb68481a29886d217965bcbde1
Ryan Murphy <murjax@gmail.com>
2016-06-09 17:52:28 +0000

    Refactoring

--- lib/vagrant_binding.rb
+++ lib/vagrant_binding.rb
@@ -1,10 +1,10 @@
 require "vagrant_binding/version"
 require 'rails/commands/server'
-
+#TODO: move code into vagrant_binding/server.rb 
 module Rails
   class Server
     def default_options
-      super.merge(Host:  '0.0.0.0', Port: 3000)
+    	super.merge(Host:  '0.0.0.0')
     end
   end
 end
--- spec/vagrant_binding_spec.rb
+++ spec/vagrant_binding_spec.rb
@@ -1,13 +1,17 @@
 require 'spec_helper'
 
 describe VagrantBinding do
-  let(:server) { Rails::Server.new }	
+  let(:server) { Rails::Server.new }
   it 'has a version number' do
     expect(VagrantBinding::VERSION).not_to be nil
   end
 
-  it 'has changed the default host' do
-  	expect(server.options[:Host]).to eq("0.0.0.0")
-  	expect(server.options[:Port]).to eq(3000)
+  context "when in development mode" do
+
+	it 'has changed the default host' do
+	  expect(Rails.env).to receive(:development?).and_return(true)
+	  expect(server.options[:Host]).to eq("0.0.0.0")
+	end
   end
+  
 end
